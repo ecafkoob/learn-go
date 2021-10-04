9fae05e29e9c6b602a5e33bcaf6ffb7d0190a688
lovefov <a654747873@gmail.com>
2021-10-04 10:26:10 +0800

    docs(ch1): dlv usage

--- ch1/main.go
+++ ch1/main.go
@@ -1,7 +1,23 @@
 package main
 
-func main() {
-	args()
+import (
+	"fmt"
+)
+
+type Iface interface {
+	Blah() string
 }
 
-func args() {}
+type A struct {
+	a int
+}
+
+func (A) Blah() string {
+	return "blah"
+}
+
+func main() {
+	var iface Iface = A{a: 1}
+	s := iface.Blah()    // line 21
+	fmt.Printf("%s\n", s)
+}
