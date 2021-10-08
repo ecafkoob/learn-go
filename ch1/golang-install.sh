532f59805a677e0d6b1c39cee88fbb9e05ecd4e7
lovefov <a654747873@gmail.com>
2021-10-08 19:33:47 +0800

    feat(ch1): install docker default

--- ch1/golang-install.sh
+++ ch1/golang-install.sh
@@ -2,6 +2,8 @@
 
 apt update && apt install vim gdb binutils -y
 
+curl -fsSL https://get.docker.com -o get-docker.sh
+
 curl -L -o go1.17.1.linux-amd64.tar.gz https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
 
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
